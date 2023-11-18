9bc09f00ff9f1d1792b913392da8409726d83f28
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-11-18 09:39:34 -0300

    feat: set up storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+
+const config: StorybookConfig = {
+  staticDirs: ['../public'],
+  stories: ['../src/components/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  webpackFinal(config) {
+    config.resolve?.modules?.push(`${process.cwd()}/src`)
+    return config
+  },
+}
+export default config
