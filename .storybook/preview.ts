b294ad45eafa548c33ff55ec30dfa0261f653488
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-11-18 09:58:45 -0300

    feat: add theme add on to storybook and use global styles to stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,4 @@
-import type { Preview } from '@storybook/react'
+import { withThemeFromJSXProvider } from '@storybook/addon-themes'
+import { GlobalStyles } from '../src/styles/global'
 
-const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
-}
-
-export default preview
+export const decorators = [withThemeFromJSXProvider({ GlobalStyles })]
