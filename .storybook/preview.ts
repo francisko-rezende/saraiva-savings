725204baabece8d87baa79fd8eff08252ed5cbbb
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-11-18 22:02:15 -0300

    chore: use theme provider in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,15 @@
+import { theme } from './../src/styles/theme'
 import { withThemeFromJSXProvider } from '@storybook/addon-themes'
 import { GlobalStyles } from '../src/styles/global'
+import { ThemeProvider } from 'styled-components'
 
-export const decorators = [withThemeFromJSXProvider({ GlobalStyles })]
+export const decorators = [
+  withThemeFromJSXProvider({
+    Provider: ThemeProvider,
+    themes: {
+      default: theme,
+    },
+    defaultTheme: 'default',
+    GlobalStyles,
+  }),
+]
