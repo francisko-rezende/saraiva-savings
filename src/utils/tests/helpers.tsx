4e239a41a0ff29a5a984ffed66a4de749ad3447b
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-02-18 19:07:43 -0300

    feat: add filters provider to test helper

--- src/utils/tests/helpers.tsx
+++ src/utils/tests/helpers.tsx
@@ -3,9 +3,14 @@ import { render, RenderResult } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 import { theme } from '@/styles/theme'
+import { FiltersProvider } from '@/components/FiltersContext/FiltersContext'
 
 export const renderWithTheme = (children: React.ReactNode): RenderResult =>
-  render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
+  render(
+    <FiltersProvider>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </FiltersProvider>,
+  )
 
 export const renderWithUser = (component: React.ReactElement) => {
   return {
